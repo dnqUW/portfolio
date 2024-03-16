12586c3923196939f96e3ab2c3042ca0aa13b8cc
Derek <dnq@uw.edu>
2024-03-15 17:13:02 -0700

    cards and projects

---
+++ src/components/project-cards.js
@@ -0,0 +1,15 @@
+import "../css/project-cards.css";
+
+
+export default function ProjectCard(props) {
+    return(
+        <div className="card">
+        {/* <img className="card-img-top" src={props.data.thumbnail} alt={props.data.alt}/> */}
+        <div className="card-body">
+            <h5 className="card-title">{props.data.name}</h5>
+            <p className="card-text">{props.data.description}</p>
+            <a href= {props.data.url} className="btn btn-primary">Go</a>
+        </div>
+        </div>        
+    );
+}
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,21 @@
 import Navbar from "../components/navbar.js";
 import Footer from "../components/footer.js";
+import ProjectData from "../data/projects.json";
+import Card from "../components/project-cards.js";
 
 export default function Projects() {
+    
+    let arrayCards = ProjectData.map((cardData) => {
+        return <Card data={cardData}/>;
+    });
+    
+
     return (
         <section>
             <Navbar />
             <h1>Projects</h1>
-            <div>
-                <p1>asdasda</p1>
+            <div className="array-cards-container">
+                {arrayCards}
             </div>
             <Footer />
         </section>
